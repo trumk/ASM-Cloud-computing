2da4280c9b3ef2409fcd86c097cd4884e354fd85
trumk <nguyentrungtest2292003@gmail.com>
2023-08-15 22:13:21 +0700

    delete route

--- code/routes/lego.js
+++ code/routes/lego.js
@@ -3,8 +3,29 @@ const LegoModel = require("../models/LegoModel");
 var router = express.Router();
 
 router.get("/", async (req, res) => {
-    var lego = await LegoModel.find()
-    res.render("toy/lego/index", { lego: lego });
-  });
+  var lego = await LegoModel.find();
+  res.render("toy/lego/index", { lego: lego });
+});
+
+router.get("/delete/:id", async (req, res) => {
+  var id = req.params.id;
+  await LegoModel.findByIdAndDelete(id)
+    .then(() => console.log("Delete successfully"))
+    .catch((error) => console.log("Delete failed"));
+  res.redirect("/toy/lego");
+});
+
+router.get("/deleteall", async (req, res) => {
+  await LegoModel.deleteMany()
+    .then(() => console.log("Delete successfully"))
+    .catch((error) => console.log("Delete failed"));
+  res.redirect("/toy/lego");
+});
+
+router.get("/add", (req, res) => {
+  res.render("student/studentAdd");
+});
+
+
 
 module.exports = router;
