77dc138852515e21a7f0a628d0359d9a839e00b3
trumk <nguyentrungtest2292003@gmail.com>
2023-08-18 19:58:28 +0700

    change index admin

--- code/routes/index.js
+++ code/routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 const CarModel = require('../models/CarModel');
+const LegoModel = require('../models/LegoModel');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async (req, res) => {
+  var car = await CarModel.find();
+  var lego = await LegoModel.find();
+  res.render('index', { car: car, lego:lego });
 });
 
 module.exports = router;
